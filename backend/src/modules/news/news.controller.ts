c825f2bd77ac064be0ea30fb55d5543923b7bd7b
100-KG <taducminh25104@gmail.com>
2026-02-08 23:25:52 +0700

    create news controller, module, service, dto folder and create/update classes to news dto

---
+++ backend/src/modules/news/news.controller.ts
@@ -0,0 +1,9 @@
+// import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+
+// @Controller('news')
+// export class NewsController{
+//   @Post()
+//   @Get()
+//   @Patch()
+//   @Delete()
+// }
