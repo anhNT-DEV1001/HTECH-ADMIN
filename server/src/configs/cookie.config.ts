cc1d3cf4387d267c17361c79d88c62cdd494fac6
AnhNT <anhtuannguyen10012004@gmail.com>
2026-04-15 00:48:14 +0700

    update

--- server/src/configs/cookie.config.ts
+++ server/src/configs/cookie.config.ts
@@ -1,5 +1,5 @@
 import { CookieOptions } from 'express';
-import { isPord } from './key.config';
+import { isPord, key } from './key.config';
 
 export const getCookieConfig = {
   accessToken: {
@@ -8,7 +8,7 @@ export const getCookieConfig = {
     sameSite: isPord ? 'none' : 'lax',
     maxAge: 1000 * 60 * 60 * 24,
     path: '/',
-    domain: isPord ? '14.225.198.222' : undefined,
+    domain: isPord ? key.app.host : undefined,
   },
   refreshToken: {
     httpOnly: true,
@@ -16,6 +16,6 @@ export const getCookieConfig = {
     sameSite: isPord ? 'none' : 'lax',
     path: '/api/v1/auth/refresh',
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    domain: isPord ? '14.225.198.222' : undefined,
+    domain: isPord ? key.app.host : undefined,
   },
 } as { accessToken: CookieOptions; refreshToken: CookieOptions };
